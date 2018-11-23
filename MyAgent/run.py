1fd1b4e47f44a12af6c921ed6e97b75c01b4c046
Brain <562966616@qq.com>
2018-11-23 16:51:31 +0800

    fix agent parameter

--- MyAgent/run.py
+++ MyAgent/run.py
@@ -30,7 +30,7 @@ class MyAgent(DockerAgentRunner):
 def main():
     '''Inits and runs a Docker Agent'''
     sess = tf.InteractiveSession()
-    path = 'MCTS_net-DRL_1'
+    path = 'MCTS_net-DRL_2'
     params = joblib.load(path)
     agent = MyAgent(sess, params)
     agent.run()
